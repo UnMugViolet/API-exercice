89e107c377c96ec7369e6303a960b3b467db64e9
Paul Jaguin <paul@screenup.com>
2024-02-27 16:27:06 +0100

    🚀 Create owner service / module

--- src/owner/owner.module.ts
+++ src/owner/owner.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OwnerService } from './owner.service';
 import { OwnerController } from './owner.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OwnerEntity } from './entities/owner.entity';
 
 @Module({
-  controllers: [OwnerController],
+  imports: [TypeOrmModule.forFeature([OwnerEntity])],
   providers: [OwnerService],
+  controllers: [OwnerController],
+  exports: [OwnerService]
 })
 export class OwnerModule {}
--- src/owner/owner.service.ts
+++ src/owner/owner.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOwnerDto } from './dto/create-owner.dto';
 import { UpdateOwnerDto } from './dto/update-owner.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OwnerEntity } from './entities/owner.entity';
+import { DeepPartial, Repository } from 'typeorm';
 
 @Injectable()
 export class OwnerService {
+  constructor(
+    @InjectRepository(OwnerEntity)
+    private readonly ownerRepository: Repository<OwnerEntity>,
+  ) {}
+
   create(createOwnerDto: CreateOwnerDto) {
-    return 'This action adds a new owner';
+    const newOwner = this.ownerRepository.create(createOwnerDto as DeepPartial<OwnerEntity>);
+    return this.ownerRepository.save(newOwner);
   }
 
   findAll() {
-    return `This action returns all owner`;
+    return this.ownerRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} owner`;
+  async findOne(id: number) {
+    return await this.ownerRepository.findOne({ where: { id } });
   }
 
-  update(id: number, updateOwnerDto: UpdateOwnerDto) {
-    return `This action updates a #${id} owner`;
+  async update(id: number, updateOwnerDto: UpdateOwnerDto) {
+    const owner = await this.ownerRepository.findOne({ where: { id } });
+    Object.assign(owner, updateOwnerDto);
+    return this.ownerRepository.save(owner);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} owner`;
+    return this.ownerRepository.delete(id);
   }
 }
