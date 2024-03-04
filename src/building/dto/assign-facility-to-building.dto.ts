6f5f225b4013eeaa826b660e594421d1318a8fec
UnMugViolet <jaguinpaul@gmail.com>
2024-03-04 17:10:31 +0100

    Fix issue

--- src/building/dto/assign-facility-to-building.dto.ts
+++ src/building/dto/assign-facility-to-building.dto.ts
@@ -2,8 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsNumber } from 'class-validator';
 
 export class AssignFacilityToBuildingDto {
-  @ApiProperty({ example: [1, 2, 3]})
-  @IsArray()
-  @IsNumber({}, { each: true })
-  facilitiesId: number[];
+  @ApiProperty({ example: 1})
+  facilitiesId: number;
 }
